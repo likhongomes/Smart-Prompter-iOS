8cdba066ba91c278b901e0a1cb96a00de320aabd
likhongomes <tug60499@temple.edu>
2019-09-05 00:02:48 -0400

    initial commit

---
+++ SmartPrompter/SmartPrompter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartPrompter
+//
+//  Created by Likhon Gomes on 9/5/19.
+//  Copyright © 2019 Likhon Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
