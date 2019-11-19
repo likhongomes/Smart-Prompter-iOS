8223f1b32f1243b8b51fcbe7474f3e86995eaad3
likhongomes <tug60499@temple.edu>
2019-11-18 23:25:07 -0500

    keyboard issue kinda fixed

--- SmartPrompter Patient/SmartPrompter/UserInfoVC.swift
+++ SmartPrompter Patient/SmartPrompter/UserInfoVC.swift
@@ -28,6 +28,24 @@ class UserInfoVC: UIViewController {
         patientLastNameTFSetup()
         doneButtonSetup()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     func informationLabelSetup() {
