b33e00614a9ac98bdcb5262ce051f5ca9ca290a5
likhongomes <tug60499@temple.edu>
2019-12-17 19:42:41 -0500

    repeat function WIP

--- SmartPrompter/SmartPrompter/AlarmScheduler.swift
+++ SmartPrompter/SmartPrompter/AlarmScheduler.swift
@@ -25,32 +25,33 @@ class AlarmScheduler {
             content.badge = 1
             content.userInfo = ["FirebaseID":id,"title":title, "hour":dateComponents.hour, "minute":dateComponents.minute,"day":dateComponents.day,"month":dateComponents.month,"year":dateComponents.year]
             
-            //var dateComponents = DateComponents()
-            //dateComponents.hour = 19
-            //dateComponents.minute = 24
-            print("scheduled alarm time \(dateComponents.hour) \(dateComponents.minute)")
-            //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+            
+            
             let calendarTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-            
-            var dateComponents2 = dateComponents
-            dateComponents2.addAMinute()
-            print("first time \(dateComponents.hour):\(dateComponents.minute) second time \(dateComponents2.hour):\(dateComponents2.minute)")
-            let calendarTrigger2 = UNCalendarNotificationTrigger(dateMatching: dateComponents2, repeats: true)
-            
             let firstRequest = UNNotificationRequest(identifier: "Identifier", content: content, trigger: calendarTrigger)
-            let repeatRequest = UNNotificationRequest(identifier: "repeatAlarm", content: content, trigger: calendarTrigger2)
-    
             UNUserNotificationCenter.current().add(firstRequest) { (error) in
                 print("Notification Report \(error?.localizedDescription)")
             }
-            
-            UNUserNotificationCenter.current().add(repeatRequest) { (error) in
-                print("Notification Report \(error?.localizedDescription)")
-            }
+
             
             UNUserNotificationCenter.current().getDeliveredNotifications { (notifications) in
                 print(notifications)
             }
+            
+            var x = 0
+            var dateComponents2 = dateComponents
+            while (x<5){
+                dateComponents2.addAMinute()
+                print("first time \(dateComponents.hour):\(dateComponents.minute) second time \(dateComponents2.hour):\(dateComponents2.minute)")
+                let calendarTrigger2 = UNCalendarNotificationTrigger(dateMatching: dateComponents2, repeats: true)
+                let repeatRequest = UNNotificationRequest(identifier: "repeatAlarm", content: content, trigger: calendarTrigger2)
+                
+                UNUserNotificationCenter.current().add(repeatRequest) { (error) in
+                    print("Notification Report \(error?.localizedDescription)")
+                }
+                x += 1
+            }
+
             
 
         } else {
